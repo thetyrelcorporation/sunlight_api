1c39a887b0b9bc1cf829779e13460a9583438947
The Tyrel Corporation <tyrel@thetyrelcorporation.com>
2015-04-02 14:30:44 -0600

    Each product complete

--- lib/sunlight_api.rb
+++ lib/sunlight_api.rb
@@ -1,5 +1,80 @@
 require "sunlight_api/version"
+require "rquest"
+require "json"
 
 module SunlightApi
-  # Your code goes here...
+	class Client
+		def initialize( public_key, private_key )
+			@public_key = public_key
+			@private_key = private_key
+		end
+
+		def request( action )
+			uri = SunlightApi::UriGenerator::new(@public_key, @private_key, action, {format: "json"}).url
+			rclient = Rquest::new({verb: :get, uri: uri})
+			body = rclient.send
+			return nil if body.class == Hash and not body['error'].nil?
+			JSON::parse( body )
+		end
+
+		def inventory
+			request("inventory")
+		end
+
+		def array_of_product_ids
+			@inventroy = inventory
+			unless @inventroy.nil?
+				@inventroy.inject([]){|r,v| r.push(v["Id"])}
+			else
+				[]
+			end
+		end
+
+		def product_info( id )
+			request("part/#{id}")
+		end
+
+		def each_product( &block )
+			array_of_product_ids.each do |product_id|
+				product = product_info( product_id )
+				next unless product
+				yield product
+			end
+		end
+	end
+	class UriGenerator
+		attr_reader :url
+		def initialize( public_key, private_key, uri_suffix, get_params={} )
+			@public_key = public_key
+			@private_key = private_key
+			@base_url = "https://services.sunlightsupply.com/v1/#{uri_suffix}?"
+			@get_params = get_params
+			@time_stamp = Time.now.utc.strftime("%Y-%m-%dT%H:%M:%SZ")
+			append_get_params
+			append_api_key
+			append_time_stamp
+			append_signature
+		end
+
+		def append_get_params
+			@get_params.each_with_index do |(key,value), i|
+				@base_url += "#{key}=#{value}"
+				@base_url += "&" unless i == (@get_params.size - 1)
+			end
+		end
+
+		def append_api_key
+			@base_url += "&" if @get_params.size > 0
+			@base_url += "X-ApiKey=#{@public_key}"
+		end
+
+		def append_time_stamp
+			@base_url += "&time=#{@time_stamp}"
+		end
+
+		def append_signature
+			@signature = Digest::HMAC.hexdigest(@base_url, @private_key, Digest::SHA256).upcase
+			@url = "#{@base_url}&signature=#{@signature}"
+		end
+	end
 end
--- spec/sunlight_api_spec.rb
+++ spec/sunlight_api_spec.rb
@@ -4,8 +4,56 @@ describe SunlightApi do
   it 'has a version number' do
     expect(SunlightApi::VERSION).not_to be nil
   end
+	let (:public_key) { ENV['sunlight_public_key'] }
+	let (:private_key) { ENV['sunlight_secret_key'] }
+	describe "Testing setup" do
+		it "has keys stored in environment variables" do
+			expect( public_key.nil? ).to be false
+			expect( private_key.nil? ).to be false
+		end
+	end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
+	describe 'UrlGenerator' do
+		it "Generates urls with proper security credentials" do
+			uri = SunlightApi::UriGenerator.new(public_key, private_key, "Part", {format: "json"}).url
+			url, q_string = uri.split("?")
+			q_params = q_string.split("&").map{|key_value_string| key_value_string.split("=")}
+			expect( url ).to eq "https://services.sunlightsupply.com/v1/Part"
+			expect( q_params.select{|e| e.first == "X-ApiKey"}.size ).to eq 1
+			expect( q_params.select{|e| e.first == "time"}.size ).to eq 1
+			expect( q_params.select{|e| e.first == "signature"}.size ).to eq 1
+		end
+	end
+
+	describe 'Client' do
+		let (:client) { SunlightApi::Client::new(public_key, private_key) }
+		describe 'ArrayOfProductIds' do
+			it "Responds with an array of product ids" do
+				array_of_product_ids = client.array_of_product_ids
+				expect( array_of_product_ids.class ).to be Array
+				expect( array_of_product_ids.first.class ).to be String
+				expect( array_of_product_ids.size ).to be > 0
+			end
+		end
+
+		describe "ProductInfo" do
+			it "Responds with hash of all product info for id" do
+				array_of_product_ids = client.array_of_product_ids
+				id = array_of_product_ids.last
+				product_info = client.product_info(id)
+				expect( product_info.class ).to be Hash
+				expect( product_info["Name"] ).not_to be nil
+			end
+		end
+
+		# Test takes forever here for api reference
+		# describe "EachProduct" do
+		# 	it "Takes a block" do
+		# 		client.each_product do |product|
+		# 			expect(product["Name"].nil?).to be false
+		# 		end
+		# 	end
+		# end
+
+	end
 end
